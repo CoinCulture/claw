7f3a086c11fe56a8df111aef8279e08c69c21bde
joshkestenberg <joshkestenberg@gmail.com>
2017-06-27 16:17:23 -0400

    removed name variable and purged all references in util.go and contracts.go; now compiling is to be done in generated directories (changes reflected in README)

--- util.go
+++ util.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-	"github.com/russross/blackfriday"
-	"github.com/spf13/viper"
+  "github.com/russross/blackfriday"
+  "github.com/spf13/viper"
 )
 
 // append to list if not in list already
 func appendNew(list []string, name string) []string {
-	found := false
-	for _, el := range list {
-		if el == name {
-			found = true
-		}
-	}
-	if !found {
-		list = append(list, name)
-	}
-	return list
+  found := false
+  for _, el := range list {
+    if el == name {
+      found = true
+    }
+  }
+  if !found {
+    list = append(list, name)
+  }
+  return list
 }
 
 // load params config
-func loadConfig(rootDir string) (*viper.Viper, error) {
-	config := viper.New()
-	config.SetConfigName("params")
-	config.SetConfigType("toml")
-	config.AddConfigPath(rootDir)
-	err := config.ReadInConfig()
-	if err != nil {
-		return nil, err
-	}
-	return config, nil
+func loadConfig() (*viper.Viper, error) {
+  config := viper.New()
+  config.SetConfigName("params")
+  config.SetConfigType("toml")
+  config.AddConfigPath(".")
+  err := config.ReadInConfig()
+  if err != nil {
+    return nil, err
+  }
+  return config, nil
 }
 
 func markdown2html(b []byte) []byte {
-	return blackfriday.MarkdownBasic(b)
+  return blackfriday.MarkdownBasic(b)
 }
