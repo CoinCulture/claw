1a012b9c648650810f3a406e934357812f01b20b
joshkestenberg <joshkestenberg@gmail.com>
2017-06-28 16:00:57 -0400

    fix gofmt

--- main.go
+++ main.go
@@ -1,49 +1,51 @@
-import (
-  "fmt"
-  "os"
+package main
 
-  "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
 // global flags
 var outputType string
 
 var RootCmd = &cobra.Command{
-  Use:   "claw",
-  Short: "Command Line Law",
-  PersistentPreRun: func(cmd *cobra.Command, args []string) {
-  },
+	Use:   "claw",
+	Short: "Command Line Law",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+	},
 }
 
 var newCmd = &cobra.Command{
-  Use:   "new",
-  Short: "New Contract",
-  RunE:  newContract,
+	Use:   "new",
+	Short: "New Contract",
+	RunE:  newContract,
 }
 
 var compileCmd = &cobra.Command{
-  Use:   "compile",
-  Short: "Compile into readable contract",
-  RunE:  compileContract,
+	Use:   "compile",
+	Short: "Compile into readable contract",
+	RunE:  compileContract,
 }
 
 var reviseCmd = &cobra.Command{
-  Use:   "revise",
-  Short: "Hash modifications to params and template",
-  RunE:  reviseContract,
+	Use:   "revise",
+	Short: "Hash modifications to params and template",
+	RunE:  reviseContract,
 }
 
 func init() {
-  compileCmd.Flags().StringVar(&outputType, "output", "md", "Output type: md | html | pdf")
+	compileCmd.Flags().StringVar(&outputType, "output", "md", "Output type: md | html | pdf")
 
-  RootCmd.AddCommand(newCmd)
-  RootCmd.AddCommand(compileCmd)
-  RootCmd.AddCommand(reviseCmd)
+	RootCmd.AddCommand(newCmd)
+	RootCmd.AddCommand(compileCmd)
+	RootCmd.AddCommand(reviseCmd)
 }
 
 func main() {
-  if err := RootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
